e5a5176c1b83c5e6fab2a4fd8af55382de3e6661
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-05-22 01:21:42 +0200

    Adapt to new Postfinance interface

--- ynab_postfinance_export.user.js
+++ ynab_postfinance_export.user.js
@@ -1,205 +1,104 @@
 // ==UserScript==
 // @name        Postfinance CSV Export
 // @namespace   Kagemaru
-// @include     https://www.postfinance.ch/ap/ba/fp/html/e-finance/assets
+// @description Export Postfinance Assets
+// @match       https://www.postfinance.ch/ap/ba/ob/html/finance/assets/movements-overview
 // @version     3.1.0
 // @grant       none
+// @downloadURL https://github.com/Kagemaru/ynab_postfinance_export/raw/master/ynab_postfinance_export.user.js
 // @updateURL   https://github.com/Kagemaru/ynab_postfinance_export/raw/master/ynab_postfinance_export.user.js
+// @run-at      context-menu
 // ==/UserScript==
 // Function definitions
-function createGenerateButton() {
-    let input = document.createElement("input");
-    input.type = "button";
-    input.value = "YNAB Export";
-    input.className = "btn";
-    input.style.setProperty('margin-left', '8px');
-    input.onclick = createExportButton;
 
-    $("input[name='data']")[0].parentNode.append(input);
-}
 
-function createExportButton() {
-    const csv = createCSV();
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const filename = "ynab-" + year + "-" + month + "-" + day + ".csv";
+class PFExport {
+  filename;
+  csv;
+
+  constructor(filename) {
+    this.filename = filename || this.default_filename();
+  }
+
+  /* Public API */
+  create() {
+    this.csv = PFCSV.file()
+
+    return this.csv
+  }
+
+  download() {
+    if (this.csv == null) {
+      console.log("csv is empty, please use create() first.")
+      return false;
+    }
 
     // create a temporary download link
     let input = document.createElement("a");
-    input.setAttribute('href', 'data:text/csv;charset=UTF-8,' + encodeURIComponent(csv));
-    input.setAttribute('download', filename);
+    input.setAttribute('href', 'data:text/csv;charset=UTF-8,' + encodeURIComponent(this.csv));
+    input.setAttribute('download', this.filename);
     input.style.display = 'none';
 
     // ...and click it to trigger the download
     document.body.appendChild(input);
     input.click();
     document.body.removeChild(input);
+  }
+
+  default_filename() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return "ynab-" + year + "-" + month + "-" + day + ".csv";
+  }
 }
 
-function createCSV() {
-    if ($("span[pf-translate='detailpages.efcreditcard.overview.AvailableAmountPrepaid']").length > 0) {
-        return createDebitCSV();
-    } else {
-        return createAccountCSV();
-    }
+class PFPage {
+  static table() { return this.selectByCY('table', 'movements-table') }
+  static rows() { return this.table().querySelectorAll('tbody > tr') }
+  static fields(row) {
+    return {
+      date: row.querySelector('[data-cy="date"] time').dateTime,
+      shortText: row.querySelector('[data-cy="shortText"]')?.querySelector('.cell__value')?.textContent || null,
+      credit: row.querySelector('[data-cy="credit"] [data-cy-fpui-amount]')?.attributes['fpa-amount']?.textContent || null,
+      debit: row.querySelector('[data-cy="debit"] [data-cy-fpui-amount]')?.attributes['fpa-amount']?.textContent || null,
+      valuta: row.querySelector('[data-cy="valuta"] time').dateTime,
+      balance: row.querySelector('[data-cy="balance"] [data-cy-fpui-amount]')?.attributes["fpa-amount"]?.textContent || null
+    };
+  }
+
+  static selectByCY(element, cy_name) {
+    return document.querySelector(`${element}[data-cy='${cy_name}']`);
+  }
 }
 
-function createAccountCSV() {
-    // acquire the data computed by the Post
-    let result = JSON.parse($("input[name='data']")[0].value);
+class PFCSV {
+  static file() {
+    const header = 'Date,Payee,Memo,Outflow,Inflow';
+    const rows = [...PFPage.rows()]
+    const mapping = rows.map((row) => { return this.fields(row) });
+    return [header, ...mapping].join("\n")
+  }
 
-    // process the content
-    let transactions = result.filter(row => row.length === 6);
-    let rows = transactions.map((row, index) => index > 0 ? sanitizeAccountRow(row) : createHeader());
+  static fields(pf_fields) {
+    const fields = PFPage.fields(pf_fields)
 
-    // join the results into a single string
-    return rows.join("\n");
+    const valuta = `"${fields.valuta}"`
+    const payee = `"${fields.shortText.replace(/\"/gi, '').replace(/\r\n?|\n/gi, ' ')}"`
+    //const category = '""'
+    const memo = '""'
+    const outflow = fields.debit?.replace(/-(.*)/, '$1')
+    const inflow = fields.credit
+
+    return [valuta, payee, memo, outflow, inflow].join(', ');
+  }
 }
 
-function createDebitCSV() {
-    // acquire the data computed by the Post
-    let result = JSON.parse($("input[name='data']")[0].value);
-
-    // process the content
-    let transactions = result.filter(row => row.length === 4 );
-    let sanitized = transactions.map((row, index) => sanitizeDebitRow(row));
-    let rows = [createHeader()].concat(sanitized);
-
-    // join the results into a single string
-    return rows.join("\n");
-}
-
-function createHeader() {
-    return 'Date,Payee,Category,Memo,Outflow,Inflow';
-}
-
-function sanitizeAccountRow(row) {
-    const date = row[4].replace(/(.*)-(.*)-(.*)/, '$3.$2.$1'); //Valutadatum statt Buchungsdatum
-    const payee = '"' + row[1].replace(/\"/gi, '').replace(/\r\n?|\n/gi, ' ')
-                              .replace(/(KAUF.DIENSTLEISTUNG|KAUF.ONLINE SHOPPING)\s*/,'')
-                              .replace(/VOM \d{2}\.\d{2}\.\d{4}\s*/, '')
-                              .replace(/KARTEN NR. XXXX\d{4}\s*/, '') + '"';
-    const category = '';
-    const memo = '';
-    const outflow = row[3].replace(/-(.*)/, '$1');
-    const inflow = row[2];
-
-    let line = [];
-    line.push(date);
-    line.push(payee);
-    line.push(category);
-    line.push(memo);
-    line.push(outflow);
-    line.push(inflow);
-
-    return line.join(',');
-}
-
-function sanitizeDebitRow(row) {
-    // 0 = date
-    // 1 = payee
-    // 2 = inflow
-    // 3 = outflow
-    const date = row[0].replace(/(.*)-(.*)-(.*)/, '$3.$2.$1'); //Valutadatum statt Buchungsdatum
-    const payee = '"' + row[1].replace(/\"/gi, '').replace(/\r\n?|\n/gi, ' ')
-                              .replace(/(KAUF.DIENSTLEISTUNG|KAUF.ONLINE SHOPPING)\s*/,'')
-                              .replace(/VOM \d{2}\.\d{2}\.\d{4}\s*/, '')
-                              .replace(/KARTEN NR. XXXX\d{4}\s*/, '') + '"';
-    const category = '';
-    const memo = '';
-    const outflow = row[3].replace(/-(.*)/, '$1');
-    const inflow = row[2];
-
-    let line = [];
-    line.push(date);
-    line.push(payee);
-    line.push(category);
-    line.push(memo);
-    line.push(outflow);
-    line.push(inflow);
-
-    return line.join(',');
-}
-
-
-
-/* waitForKeyElements():  A handy, utility function that
- * does what it says.
- */
-function waitForKeyElements(
-    selectorTxt,    /* Required: The jQuery selector string that
-                        specifies the desired element(s).
-                    */
-    actionFunction, /* Required: The code to run when elements are
-                        found. It is passed a jNode to the matched
-                        element.
-                    */
-    bWaitOnce,      /* Optional: If false, will continue to scan for
-                        new elements even after the first match is
-                        found.
-                    */
-    iframeSelector  /* Optional: If set, identifies the iframe to
-                        search.
-                    */
-) {
-    var targetNodes, btargetsFound;
-
-    if (typeof iframeSelector == "undefined")
-        targetNodes = $(selectorTxt);
-    else
-        targetNodes = $(iframeSelector).contents()
-            .find(selectorTxt);
-
-    if (targetNodes && targetNodes.length > 0) {
-        /*--- Found target node(s).  Go through each and act if they
-            are new.
-        */
-        targetNodes.each(function () {
-            var jThis = $(this);
-            var alreadyFound = jThis.data('alreadyFound') || false;
-
-            if (!alreadyFound) {
-                //--- Call the payload function.
-                actionFunction(jThis);
-                jThis.data('alreadyFound', true);
-            }
-        });
-        btargetsFound = true;
-    }
-    else {
-        btargetsFound = false;
-    }
-
-    //--- Get the timer-control variable for this selector.
-    var controlObj = waitForKeyElements.controlObj || {};
-    var controlKey = selectorTxt.replace(/[^\w]/g, "_");
-    var timeControl = controlObj[controlKey];
-
-    //--- Now set or clear the timer as appropriate.
-    if (btargetsFound && bWaitOnce && timeControl) {
-        //--- The only condition where we need to clear the timer.
-        clearInterval(timeControl);
-        delete controlObj[controlKey]
-    }
-    else {
-        //--- Set a timer, if needed.
-        if (!timeControl) {
-            timeControl = setInterval(function () {
-                waitForKeyElements(selectorTxt,
-                    actionFunction,
-                    bWaitOnce,
-                    iframeSelector
-                );
-            },
-                500
-            );
-            controlObj[controlKey] = timeControl;
-        }
-    }
-    waitForKeyElements.controlObj = controlObj;
-}
-
-// Main entry point
-waitForKeyElements("input[name='data']", createGenerateButton);
+(function () {
+  'use strict';
+  const pf_export = new PFExport()
+  pf_export.create()
+  pf_export.download()
+})();
